4418d8696e2deaab698d2d97f8fcdf322f528f7a
Gustavo Batistela <gustavoabatistela@gmail.com>
2023-09-12 20:27:19 -0300

    feat: encryption functions and tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,67 @@
 //! u8 slice. They return owned objects to preserve the original data.
 
 use aes::{
-    cipher::{generic_array::GenericArray, BlockDecrypt, KeyInit, BlockEncrypt},
+    cipher::{generic_array::GenericArray, BlockDecrypt, BlockEncrypt, KeyInit},
     Aes128, Block,
 };
 
 /// AES-128 in CBC mode with PKCS5 padding scheme
 pub struct Aes128CBC {
-    key: Block,
+    aes: Aes128,
     iv: Block,
 }
 
 impl Aes128CBC {
     pub fn new(key: &Block, iv: &Block) -> Self {
-        Self { key: *key, iv: *iv }
+        Self {
+            aes: Aes128::new(key),
+            iv: *iv,
+        }
     }
 
     pub fn set_key(&mut self, key: &Block) {
-        self.key = *key;
+        self.aes = Aes128::new(key);
     }
 
     pub fn set_iv(&mut self, iv: &Block) {
         self.iv = *iv;
+    }
+
+    /// Returns an owned vector of bytes with PKCS5 padding applied
+    fn apply_pkcs5_padding(plain_text: &[u8]) -> Vec<u8> {
+        let padding = (16 - plain_text.len() % 16) as u8;
+        plain_text
+            .iter()
+            .chain(std::iter::repeat(&padding).take(padding as usize))
+            .copied()
+            .collect()
+    }
+
+    pub fn encrypt(&self, plain_text: &[u8]) -> Vec<u8> {
+        let padded_plain_text = Self::apply_pkcs5_padding(plain_text);
+        let mut blocks: Vec<Block> = padded_plain_text
+            .chunks(16)
+            .map(GenericArray::clone_from_slice)
+            .collect();
+        self.encrypt_blocks(blocks.iter_mut())
+    }
+
+    fn encrypt_blocks<'a, I>(&self, blocks: I) -> Vec<u8>
+    where
+        I: Iterator<Item = &'a mut Block>,
+    {
+        let mut prev = self.iv;
+        let mut cipher_text = Vec::new();
+
+        for block in blocks {
+            for (i, byte) in block.iter_mut().enumerate() {
+                *byte ^= prev[i];
+            }
+            self.aes.encrypt_block(block);
+            prev = *block;
+            cipher_text.extend_from_slice(block);
+        }
+        cipher_text
     }
 
     pub fn decrypt(&self, cipher_text: &[u8]) -> Vec<u8> {
@@ -36,47 +76,52 @@ impl Aes128CBC {
         self.decrypt_blocks(blocks.iter_mut())
     }
 
-    pub fn decrypt_blocks<'a, I>(&self, blocks: I) -> Vec<u8>
+    fn decrypt_blocks<'a, I>(&self, blocks: I) -> Vec<u8>
     where
         I: Iterator<Item = &'a mut Block>,
     {
-        let aes = Aes128::new(&self.key);
         let mut prev = self.iv;
         let mut plain_text = Vec::new();
 
         for block in blocks {
             let new_prev = *block;
-            aes.decrypt_block(block);
+            self.aes.decrypt_block(block);
             for (i, byte) in block.iter_mut().enumerate() {
                 *byte ^= prev[i];
             }
             prev = new_prev;
             plain_text.extend_from_slice(block);
         }
+        // Remove padding and return
+        let padding = plain_text[plain_text.len() - 1];
+        plain_text.truncate(plain_text.len() - padding as usize);
         plain_text
     }
 }
 
 /// AES-128 in CTR mode with PKCS5 padding scheme
 pub struct Aes128CTR {
-    key: Block,
+    aes: Aes128,
     iv: Block,
 }
 
 impl Aes128CTR {
     pub fn new(key: &Block, iv: &Block) -> Self {
-        Self { key: *key, iv: *iv }
+        Self {
+            aes: Aes128::new(key),
+            iv: *iv,
+        }
     }
 
     pub fn set_key(&mut self, key: &Block) {
-        self.key = *key;
+        self.aes = Aes128::new(key);
     }
 
     pub fn set_iv(&mut self, iv: &Block) {
         self.iv = *iv;
     }
 
-    pub fn decrypt(&self, cipher_text: &[u8]) -> Vec<u8> {
+    pub fn encrypt(&self, cipher_text: &[u8]) -> Vec<u8> {
         let mut extension = 0;
         let mut blocks: Vec<Block> = cipher_text
             .chunks(16)
@@ -91,16 +136,15 @@ impl Aes128CTR {
                 }
             })
             .collect();
-        let mut result = self.decrypt_blocks(blocks.iter_mut());
+        let mut result = self.encrypt_blocks(blocks.iter_mut());
         result.truncate(result.len() - extension);
         result
     }
 
-    pub fn decrypt_blocks<'a, I>(&self, blocks: I) -> Vec<u8>
+    fn encrypt_blocks<'a, I>(&self, blocks: I) -> Vec<u8>
     where
         I: Iterator<Item = &'a mut Block>,
     {
-        let aes = Aes128::new(&self.key);
         let (nonce, counter) = self.iv.split_at(8);
         let mut counter_u64 = u64::from_be_bytes(counter[..8].try_into().unwrap());
         let mut plain_text = Vec::new();
@@ -110,7 +154,7 @@ impl Aes128CTR {
             let mut counter_block = Block::default();
             counter_block[..8].copy_from_slice(nonce);
             counter_block[8..].copy_from_slice(&counter_u64.to_be_bytes());
-            aes.encrypt_block(&mut counter_block);
+            self.aes.encrypt_block(&mut counter_block);
             for (i, byte) in block.iter_mut().enumerate() {
                 *byte ^= counter_block[i];
             }
@@ -118,5 +162,53 @@ impl Aes128CTR {
             plain_text.extend_from_slice(block);
         }
         plain_text
+    }
+
+    pub fn decrypt(&self, plain_text: &[u8]) -> Vec<u8> {
+        self.encrypt(plain_text)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_input_data() -> Vec<&'static [u8]> {
+        vec![
+            &hex_literal::hex!("0123456789abcdef0123456789abcdef"),
+            "".as_bytes(),
+            "a".as_bytes(),
+            "It's dangerous to go alone, take this!".as_bytes(),
+        ]
+    }
+
+    #[test]
+    fn test_cbc() {
+        let key = hex_literal::hex!("0123456789abcdef0123456789abcdef");
+        let iv = hex_literal::hex!("00001111222233334444555566667777");
+        let aes_cbc = Aes128CBC::new(
+            GenericArray::from_slice(&key),
+            GenericArray::from_slice(&iv),
+        );
+        for input in get_input_data() {
+            let cipher_text = aes_cbc.encrypt(&input);
+            let decrypted_text = aes_cbc.decrypt(&cipher_text);
+            assert_eq!(input, decrypted_text);
+        }
+    }
+
+    #[test]
+    fn test_ctr() {
+        let key = hex_literal::hex!("0123456789abcdef0123456789abcdef");
+        let iv = hex_literal::hex!("00001111222233334444555566667777");
+        let aes_ctr = Aes128CTR::new(
+            GenericArray::from_slice(&key),
+            GenericArray::from_slice(&iv),
+        );
+        for input in get_input_data() {
+            let cipher_text = aes_ctr.decrypt(&input);
+            let decrypted_text = aes_ctr.encrypt(&cipher_text);
+            assert_eq!(input, decrypted_text);
+        }
     }
 }
